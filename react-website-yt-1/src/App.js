ae11199dd87047bbd055db83bd12902b179f5481
vinhhiep96 <duyenhai.tvvn@gmail.com>
2022-05-13 12:02:15 +0700

    first commit

---
+++ react-website-yt-1/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Router>   
+        <Navbar />       
+        <Routes>
+          <Route path='/' exact />    
+        </Routes>                
+      </Router>
+    </>        
+  );
+}
+
+export default App;
